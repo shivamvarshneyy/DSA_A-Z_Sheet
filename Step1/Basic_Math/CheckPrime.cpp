8b85f1041eedd9520849e5cc7085cdceebfa84fa
shivamvarshneyy <shivamvarshney1506@gmail.com>
2025-09-28 15:24:12 +0530

    isprime header file added

---
+++ Step1/Basic_Math/CheckPrime.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#include"../../Header_File/isPrime.h"
+using namespace std;
+class MySolution{
+public:
+   bool checkPrimeNumber(int n){
+        isPrime prime;
+        return prime.checkPrime(n);
+   }
+};
+int main(){
+   MySolution* obj = new MySolution;
+   int n;
+   cout<<"Enter a Number"<<endl;
+   cin>>n;
+   int res = obj -> checkPrimeNumber(n);
+   if(res)cout<<"true";
+   else cout<<"false";
+   delete obj;
+   return 0;
+}
