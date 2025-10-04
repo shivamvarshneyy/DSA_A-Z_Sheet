881742805cb6bc4949ca61d31ea529c3da6e900b
shivamvarshneyy <shivamvarshney1506@gmail.com>
2025-10-04 15:43:24 +0530

    Binary Search

---
+++ Step4-(BinarySearch)/1D-Array/BinarySearch.cpp
@@ -0,0 +1,76 @@
+/*
+
+Binary Search
+
+Problem statement: You are given a sorted array of integers and a target,
+                   your task is to search for the target in the given array.
+                   Assume the given array does not contain any duplicate numbers.
+
+Example :          Let’s say the given array is = {3, 4, 6, 7, 9, 12, 16, 17} and target = 6,
+                   then output will be "true" or if we find position then output will be 2.
+                   in case element is not present then it will return false or -1 as output.
+
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+| Approach                    | Code Function   | Time Complexity | Space Complexity | Output                    | Pros                           | Cons                                     |
+| --------------------------- | --------------- | --------------- | ---------------- | ------------------------- | ------------------------------ | ---------------------------------------- |
+| **Iterative Binary Search** | `binarySearch1` | **O(log n)**    | **O(1)**         | Returns **index** (or -1) | Full control, returns position | More lines of code                       |
+| **STL `binary_search`**     | `binarySearch2` | **O(log n)**    | **O(1)**         | Returns **true/false**    | Concise, easy to use           | Doesn’t return position (only existence) |
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class MySolution {
+public:
+    int binarySearch1(vector<int>& arr, int target, int n){
+        int s = 0;
+        int e = n-1;
+        int mid = s + (e-s)/2;
+        while(s<=e){
+            if(arr[mid] == target)return mid;
+            else if(arr[mid] > target)e = mid - 1;
+            else s = mid + 1;
+            mid = s + (e-s)/2;
+        }
+        return -1;
+    }
+    int binarySearch2(vector<int>& arr, int target, int n){
+        return binary_search(arr.begin(), arr.end(), target);
+    }
+};
+
+int main() {
+    MySolution* obj = new MySolution;
+
+    int n;
+    cout << "Enter array size:" << endl;
+    
+    while (true) {
+        cin >> n;
+        if (n > 0) break;
+        cout << "Invalid size. Re-enter array size: ";
+    }
+
+    vector<int> arr(n);
+    cout << "Enter array elements:" << endl;
+    for (int i = 0; i < n; i++) cin >> arr[i];
+
+    cout << "Enter target elements:" << endl;
+    int target;
+    cin >> target;
+
+    int a = obj->binarySearch1(arr, target, n);
+    if (a != -1) cout << "The element is present at index " << a << " in array." << endl;
+    else cout << "The element is not present in array." << endl;
+
+
+    int b = obj->binarySearch2(arr, target, n);
+    if(b)cout<<"true"<<endl;
+    else cout<<"false"<<endl;
+
+    delete obj;
+    return 0;
+}
---
+++ Step4-(BinarySearch)/1D-Array/LowerBound.cpp
@@ -0,0 +1,80 @@
+// Implement Lower Bound
+
+// Problem Statement: Given a sorted array of N integers and an integer x, write a program to find the lower bound of x.
+
+// Examples
+
+// Example 1:
+// Input Format: N = 4, arr[] = {1,2,2,3}, x = 2
+// Result: 1
+// Explanation: Index 1 is the smallest index such that arr[1] >= x.
+
+// Example 2:
+// Input Format: N = 5, arr[] = {3,5,8,15,19}, x = 9
+// Result: 3
+// Explanation: Index 3 is the smallest index such that arr[3] >= x.
+ 
+/************************************************************************************************************************************************************************** */
+// | **Method**                 | **What it Returns**                | **Time Complexity** | **Space Complexity** | **Notes**                                                |
+// | -------------------------- | ---------------------------------- | ------------------- | -------------------- | -------------------------------------------------------- |
+// | **Brute Force**            | Index of first element `>= target` | O(N)                | O(1)                 | Simple but slow for large N                              |
+// | **Binary Search (Manual)** | Index of first element `>= target` | O(log N)            | O(1)                 | Efficient and correct                                    |
+// | **STL `lower_bound`**      | Index of first element `>= target` | O(log N)            | O(1)                 | Short and neat, returns iterator → must convert to index |
+/*************************************************************************************************************************************************************************** */ 
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class MySolution {
+public:
+    int lowerBound1(vector<int>& arr, int target, int n){
+        int ans = -1;
+        int s = 0;
+        int e = n-1;
+        int mid = s + (e-s)/2;
+        while(s<=e){
+            if(arr[mid] >= target){
+                ans = mid;
+                e = mid-1;
+            }else{
+                s = mid+1;
+            }
+            mid = s + (e-s)/2;
+        }
+        return ans;
+    }
+    int lowerBound2(vector<int>& arr, int target, int n){
+        return lower_bound(arr.begin(), arr.end(), target) - arr.begin();
+    }
+};
+
+int main() {
+    MySolution* obj = new MySolution;
+
+    int n;
+    cout << "Enter array size:" << endl;
+    
+    while (true) {
+        cin >> n;
+        if (n > 0) break;
+        cout << "Invalid size. Re-enter array size: ";
+    }
+
+    vector<int> arr(n);
+    cout << "Enter array elements:" << endl;
+    for (int i = 0; i < n; i++) cin >> arr[i];
+
+    cout << "Enter target elements:" << endl;
+    int target;
+    cin >> target;
+
+    int a = obj->lowerBound1(arr, target, n);
+    cout<<"Lower Bound of array is : "<<a<<endl;
+
+    int b = obj->lowerBound2(arr, target, n);
+    cout<<"Lower Bound of array is : "<<b<<endl;
+
+    delete obj;
+    return 0;
+}
