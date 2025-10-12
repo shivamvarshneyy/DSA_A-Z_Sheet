c50f82f362122ad029b7a7b13160f83119216fe9
shivamvarshneyy <shivamvarshney1506@gmail.com>
2025-10-12 16:40:29 +0530

    add header file

---
+++ Header_File/LowerBound.h
@@ -0,0 +1,27 @@
+#include <vector>
+
+template <typename T>
+class LowerBound{
+public:
+    static int lowerBound(const std::vector<T>& arr, T target){
+        int n = arr.size();
+        int low = 0;
+        int high = n-1;
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            if(arr[mid] < target)low = mid + 1;
+            else high = mid - 1;
+        }
+        return low;
+    }
+    static int arrayLowerBound(const T *arr, int n, T target){
+        int low = 0;
+        int high = n-1;
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            if(arr[mid] < target)low = mid + 1;
+            else high = mid - 1;
+        }
+        return low;
+    }
+};
