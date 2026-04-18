ef394e0d216620eb6f02c7407f8e886073831f5e
shivamvarshneyy <shivamvarshney1506@gmail.com>
2026-04-18 16:22:04 +0530

    Add fn to MERGE_INTERVAL

--- Step3-(ARRAYS)/Hard/MergeOverLappingIntervals.cpp
+++ Step3-(ARRAYS)/Hard/MergeOverLappingIntervals.cpp
@@ -38,6 +38,20 @@ public:
         }
         return ans;
     }
+    vector<vector<int>> merge(vector<vector<int>>& arr) {
+        int n = arr.size();
+        sort(arr.begin(),arr.end());
+        vector<vector<int>> ans;
+        ans.push_back(arr[0]);
+        for(int i=1;i<n;i++){
+            if(ans.back()[1] >= arr[i][0]){
+                ans.back()[1] = max(ans.back()[1],arr[i][1]);
+            }else{
+                ans.push_back(arr[i]);
+            }
+        }
+        return ans;
+    }
 };
 
 int main() {
